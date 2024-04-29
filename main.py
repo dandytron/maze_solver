142be80bf864332f08dc17e0c9443377a8c9186a
dandytron <104713695+dandytron@users.noreply.github.com>
2024-04-29 20:57:10 +0800

    Cleaning up syntax as per Python convention for classes

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from tkinter import Tk, BOTH, Canvas
-from window import Window
+from graphics import Window
 
-win = Window(800, 600)
-win.wait_for_close()
+def main():
+    win = Window(800, 600)
+    win.wait_for_close()
+
+main()
