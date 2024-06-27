a4f89acfe450694e7e57ddbfb0f3afcb2ff26add
dandytron <104713695+dandytron@users.noreply.github.com>
2024-06-27 19:47:18 +0800

    4: Drawing Some Lines (added line, point classes)

--- graphics.py
+++ graphics.py
@@ -1,30 +1,56 @@
 from tkinter import Tk, BOTH, Canvas
 
 class Window:
-    def __init__(self, width, height) -> None:
-        # constructor should take a width and height. This will be the size of the new window we create in pixels.
-        self.width = width
-        self.height = height
-        
+    # constructor should take a width and height. This will be the size of the new window we create in pixels.       
+    def __init__(self, width, height):         
         # should create a new root widget using Tk() and save it as a data member
-        self.root = Tk()
+        self.__root = Tk()
+
         # Set the title property of the root widget
-        self.root.title("TBD Title")
+        self.__root.title("Maze Solver")
         
         # Create a Canvas widget and save it as a data member.
-        self.canvas = Canvas()
+        self.__canvas = Canvas(self.__root, bg="white", height=height, width=width)
+
         # Pack the canvas widget so that it's ready to be drawn
-        self.canvas.pack()
+        self.__canvas.pack(fill=BOTH, expand=1)
 
         # Create a data member to represent that the window is "running", and set it to False
-        self.window_is_running = False
+        self.__running = False
+
+        # to call the protocol method on the root widget, to connect your close method to the "delete window" action. 
+        # This will stop your program from running when you close the graphical window.
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
     
+    def draw_line(self, line, fill_color="black"):
+        line.draw(self.__canvas, fill_color)
+
     # method we can call that will redraw all the graphics in the window
-    def redraw():
-        self.root.update_idletasks()
-        self.root.update()
+    def redraw(self):
+        self.__root.update_idletasks()
+        self.__root.update()
     
-    def wait_for_close():
-        self.window_is_running = True
+    def wait_for_close(self):
+        self.__running = True
+        while self.__running:
+            self.redraw()
+        print("Window closed.")
     
-    def close():
+    def close(self):
+        self.__running = False
+    
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class Line:
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
+    
+    def draw(self, canvas, fill_color):
+        canvas.create_line(
+            self.p1.x, self.p1.y, self.p2.x, self.p2.y, fill=fill_color, width=2
+        )
+    
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from graphics import Window
+from graphics import Window, Line, Point
 
 def main():
     win = Window(800, 600)
+    l = Line(Point(50,50), Point(200,200))
+    win.draw_line(l, "red")
     win.wait_for_close()
 
 main()
