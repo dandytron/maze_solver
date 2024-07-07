fb26e58e9906ac5d04b4850eaf16814812f4804a
dandytron <104713695+dandytron@users.noreply.github.com>
2024-07-07 18:31:16 +0800

    Added a Maze class that will print cells of a maze

---
+++ maze.py
@@ -0,0 +1,63 @@
+from cell import Cell
+from graphics import Window
+import time
+
+class Maze:
+    def __init__(self,
+                 x1,
+                 y1,
+                 num_rows,
+                 num_cols,
+                 cell_size_x,
+                 cell_size_y,
+                 win,
+                 ):
+        self._x1 = x1
+        self._y1 = y1
+        self._num_rows = num_rows
+        self._num_cols = num_cols
+        self._cell_size_x = cell_size_x
+        self._cell_size_y = cell_size_y
+        self._win = win
+        self._cells = []
+
+        self._create_cells()
+    
+    # This method should fill a self._cells list with lists of cells. 
+    # Each top-level list is a column of Cell objects. Once the matrix is 
+    # populated it should call its _draw_cell() method on each Cell.
+    def _create_cells(self):
+        for i in range(self._num_cols):
+            col_cells = []
+            for j in range(self._num_rows):
+                col_cells.append(Cell(self.win))
+            self._cells.append(col_cells)
+
+        # calling _draw_cell() on each of the cells in the matrix
+        for i in range(self._num_cols):
+            for j in range(self._num_rows):
+                self._draw_cell(i, j)
+
+    # This method should calculate the x/y position of the Cell based on i, j, the cell_size,
+    #  and the x/y position of the Maze itself. The x/y position of the maze represents 
+    # how many pixels from the top and left the maze should start from the side of the window.
+
+    def _draw_cell(self, i, j):
+        if self._win is None:
+            return
+        x1 = self._x1 + i * self._cell_size_x
+        y1 = self._y1 + j * self._cell_size_y
+        x2 = x1 + self._cell_size_x
+        y2 = y1 + self._cell_size_y
+        self._cells[i][j].draw(x1, y1, x2, y2)
+        self._animate()
+
+    # The animate method is what allows us to visualize what the algorithms are doing in real time. 
+    # It should simply call the window's redraw() method, then sleep for a short amount of time
+    # so your eyes keep up with each render frame. I slept for 0.05 seconds.
+
+    def _animate(self):
+        if self._win is None:
+            return
+        self._win.redraw()
+        time.sleep(0.05)
